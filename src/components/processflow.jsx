42b90a49eb9385c64da6363a31c1f950b36dead5
raoakash01 <raoakash269@gmail.com>
2025-02-06 22:41:28 +0530

    v5.5

---
+++ src/components/processflow.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './processflow.css'
+
+function ProcessFlow() {
+    return (
+        <div className="process-flow"></div>
+    )
+}
+
+export default ProcessFlow
