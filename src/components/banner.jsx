6cfd183671d54b2b5332ff7a0ce26e567779f9f9
raoakash01 <raoakash269@gmail.com>
2025-02-15 22:59:37 +0530

    v29

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './banner.css'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook, faTwitter, faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
 function Banner() {
     return (
@@ -9,7 +11,12 @@ function Banner() {
             <p className='banner-text'>The banner is to inform visitors of an important message</p>
             </div>
             <div className='banner-right'>
-            <p className='banner-text'>social icons</p>
+            <p className='banner-text'>
+                <FontAwesomeIcon className='social-icon-b' icon={faFacebook} style={{color: "#ffffff", marginRight: "1.1rem"}} />
+                <FontAwesomeIcon className='social-icon-b'  icon={faTwitter} style={{color: "#ffffff", marginRight: "1.1rem"}} />
+                <FontAwesomeIcon className='social-icon-b' icon={faLinkedin} style={{color: "#ffffff", marginRight: "1.1rem"}} />
+                <FontAwesomeIcon  className='social-icon-b' icon={faInstagram} style={{color: "#ffffff"}} />
+            </p>
             </div>
         </div>
         </>
