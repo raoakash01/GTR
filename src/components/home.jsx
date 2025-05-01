c738af5ee74a11f2d2638ef09bd5252f1b35301b
raoakash01 <raoakash269@gmail.com>
2025-05-01 17:25:42 +0530

    v173

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import "./home.css";
 import Whatsapp from "../assets/Whatsapp.png";
-import img1 from "../assets/1.webp";
-import img2 from "../assets/2.webp";
-import img3 from "../assets/4.webp";
-import img4 from "../assets/8.webp";
+import img1 from "../assets/1.webp?fm=webp&q=60";
+import img2 from "../assets/2.webp?fm=webp&q=60";
+import img3 from "../assets/4.webp?fm=webp&q=60";
+import img4 from "../assets/8.webp?fm=webp&q=60";
 
 function Home() {
     const [images, setImages] = useState([]);
